af7a9f197befd77ba8e5c60105f1ae77c22bb558
Антон Кузнецов <kuanse91@gmail.com>
2023-12-08 00:19:11 +0300

    Add workflow with update 0_2

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -110,7 +110,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated', 
+        'rest_framework.permissions.IsAuthenticated',
     ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
@@ -119,5 +119,4 @@ REST_FRAMEWORK = {
 
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
-
 }
