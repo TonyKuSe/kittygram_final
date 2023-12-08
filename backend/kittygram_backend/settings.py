6ba7734277d361132594498310cccebe183706ea
Антон Кузнецов <kuanse91@gmail.com>
2023-12-09 01:37:18 +0300

    Add workflow with update 0_2

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
-DEBUG = os.environ['DEBUG'].lower() == 'true'
+DEBUG = os.environ['DEBUG'].lower()
 
 ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS'].split(',')
 
